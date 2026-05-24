0dd13f8ce3a851ecb657b7cc243d11a723aab4bc
Mr.shadow <104501999+Gosheto1234@users.noreply.github.com>
2026-05-24 20:30:51 +0300

    Add low-resolution support

--- main.cpp
+++ main.cpp
@@ -41,6 +41,7 @@ float volume = 1.0f;
 
 FilePathList music_files;
 
+bool low_res;
 
 void directory_check()
 {
@@ -71,7 +72,7 @@ void get_music_files()
 int max_files = music_files.count;
 
 //TODO REFACTOR UI AND MAKE MORE RESPONSIVE VOLUME SLIDER
-//TODO MAKE PORT FOR 1080p MONITORS
+
 
 
 
@@ -87,7 +88,10 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Media player");//TODO: invent a name
     //MaximizeWindow();
     
-    
+    if(GetScreenWidth() == 1920 && GetScreenHeight() == 1080)
+    {
+        low_res = true;
+    }
     
     
     //AUDIO INITIALIZATION
@@ -111,7 +115,7 @@ int main(void)
     
     
 
-
+    //ToggleBorderlessWindowed();
     SetTargetFPS(165);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -134,96 +138,196 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(BLACK);
+            if(low_res)
+            {
+                DrawText("MUSIC PLAYER", 1200, 100, 20, WHITE);
+                DrawText("CLICK YOUR FAVOURITE MUSIC TO START PLAYING IT <3", 50, 200, 20, WHITE);
+            }
+            else{
             DrawText("MUSIC PLAYER", 1500, 100, 20, WHITE);
             DrawText("CLICK YOUR FAVOURITE MUSIC TO START PLAYING IT <3", 300, 200, 20, WHITE);
-            
-           
-            
-           
-            for(int j = 0; j <= 100; j++)
-            {
-                DrawRectangle(1350 , 600 + (j + 1) , 20, 10 , WHITE);
             }
-            
+            if(debug == true)
+            {                
+                if(low_res)
+                {
+                    DrawText("low res enabled", 300 , 500 , 20 , RED);
+                }
+            }
+            if(low_res)
+            {
+               for(int j = 0; j <= 100; j++)
+                {
+                    DrawRectangle(900 , 600 + (j + 1) , 20, 10 , WHITE);
+                } 
+            }
+            else
+            {
+                
+                for(int j = 0; j <= 100; j++)
+                {
+                    DrawRectangle(1350 , 600 + (j + 1) , 20, 10 , WHITE);
+                }
+            }
              
              
             //DrawCircle(1360, sila_zvuk, 20, GRAY); 
-            
-            if(CheckCollisionPointCircle(mishka, {1360 , sila_zvuk} , 20))
+            if(low_res)
             {
-                if(IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+                if(CheckCollisionPointCircle(mishka, {910 , sila_zvuk} , 20))
                 {
-                    //logic yoy
-                    if(mishka.y > sila_zvuk)
+                    if(IsMouseButtonDown(MOUSE_LEFT_BUTTON))
                     {
-                        sila_zvuk++;
-                        volume -= 0.01f;
-                    }
-                    else if(mishka.y < sila_zvuk)
-                    {
-                        sila_zvuk--;
-                        volume += 0.01f;
-                    }
-                    if(sila_zvuk <= 600)
-                    {
-                        sila_zvuk = 600;
-                        
-                    }
-                    else if(sila_zvuk >= 700)
-                    {
-                        sila_zvuk = 700;
-                    }
-                    if (volume < 0.0f) 
-                    {
-                        volume = 0.0f;
-                    }
-                    else if (volume > 1.0f) 
-                    {
-                        volume = 1.0f;
-                    }
+                        //logic only changes are in X when drawing 
+                        if(mishka.y > sila_zvuk)
+                        {
+                            sila_zvuk++;
+                            volume -= 0.01f;
+                        }
+                        else if(mishka.y < sila_zvuk)
+                        {
+                            sila_zvuk--;
+                            volume += 0.01f;
+                        }
+                        if(sila_zvuk <= 600)
+                        {
+                            sila_zvuk = 600;
+                        }
+                        else if(sila_zvuk >= 700)
+                        {
+                            sila_zvuk = 700;
+                        }
+                        if (volume < 0.0f) 
+                        {
+                            volume = 0.0f;
+                        }
+                        else if (volume > 1.0f) 
+                        {
+                            volume = 1.0f;
+                        }
                     
                     
+                    }
                 }
+            DrawCircle(910, sila_zvuk, 20, GRAY);
             }
-           DrawCircle(1360, sila_zvuk, 20, GRAY); 
+            else
+            {
+            
+                if(CheckCollisionPointCircle(mishka, {1360 , sila_zvuk} , 20))
+                {
+                    if(IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+                    {
+                        //logic yoy
+                        if(mishka.y > sila_zvuk)
+                        {
+                            sila_zvuk++;
+                            volume -= 0.01f;
+                        }
+                        else if(mishka.y < sila_zvuk)
+                        {
+                            sila_zvuk--;
+                            volume += 0.01f;
+                        }
+                        if(sila_zvuk <= 600)
+                        {
+                            sila_zvuk = 600;          
+                        }
+                        else if(sila_zvuk >= 700)
+                        {
+                            sila_zvuk = 700;
+                        }
+                        if (volume < 0.0f) 
+                        {
+                            volume = 0.0f;
+                        }
+                        else if (volume > 1.0f) 
+                        {
+                            volume = 1.0f;
+                        }
+                    
+                    
+                    }
+                }
+            DrawCircle(1360, sila_zvuk, 20, GRAY);
+            }
            
            
            
            SetMusicVolume(muzika, volume);
-           DrawText(TextFormat("Music volume: %0.1f", volume ) , 190 , 450 , 20 ,WHITE);
+           if(low_res)
+           {
+               DrawText(TextFormat("Music volume: %0.1f", volume ) , 50 , 450 , 20 ,WHITE);
+           }
+           else
+           {
+                DrawText(TextFormat("Music volume: %0.1f", volume ) , 190 , 450 , 20 ,WHITE);
+           }
            
-           
-            
-            DrawRectangle(1400, 200, 600, 700, GRAY);
-            if(enable_pause == true)
-            {
-                DrawCircle(1700 , 1000 , 60 , GRAY);
+           if(low_res)
+           {
+                DrawRectangle(1000, 150, 600, 700, GRAY);
+                if(enable_pause == true)
+                {
+                    DrawCircle(1300 , 950 , 60 , GRAY);
                 if(pause == false)
                 {
                     //DrawTriangle({1670 , 950}, {1670 , 1050} , {1750 , 1000} , RED);
-                    DrawRectangle(1670 , 970 , 20, 60 , BLACK);
-                    DrawRectangle(1710 , 970 , 20, 60 , BLACK);
-                    if(CheckCollisionPointCircle(mishka, {1700 , 1000} , 60))
+                    DrawRectangle(1270 , 920 , 20, 60 , BLACK);
+                    DrawRectangle(1310 , 920 , 20, 60 , BLACK);
+                    if(CheckCollisionPointCircle(mishka, {1300 , 900} , 60))//60 was originally
                         {
                             if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
                             {
                                 pause = true;
                             }
                         }
-                }
-                else if (pause == true)
-                {
-                    DrawTriangle({1680 , 970}, {1680 , 1030} , {1730 , 1000} , BLACK);
-                    if(CheckCollisionPointCircle(mishka, {1700 , 1000} , 60))
+                    }
+                    else if (pause == true)
+                    {
+                    DrawTriangle({1280 , 920}, {1280 , 980} , {1330 , 950} , BLACK);
+                    if(CheckCollisionPointCircle(mishka, {1300 , 900} , 60)) //TODO FIX BUTTON HITBOX and for higher res also
                         {
                             if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
                             {
                                 pause = false;
                             }
                         }
+                    }
                 }
             }
-            
+            else
+            {
+                DrawRectangle(1400, 200, 600, 700, GRAY);
+                if(enable_pause == true)
+                {
+                    DrawCircle(1700 , 1000 , 60 , GRAY);
+                    if(pause == false)
+                    {
+                        //DrawTriangle({1670 , 950}, {1670 , 1050} , {1750 , 1000} , RED);
+                        DrawRectangle(1670 , 970 , 20, 60 , BLACK);
+                        DrawRectangle(1710 , 970 , 20, 60 , BLACK);
+                        if(CheckCollisionPointCircle(mishka, {1700 , 1000} , 60))
+                        {
+                            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                            {
+                                pause = true;
+                            }
+                        }
+                    }
+                    else if (pause == true)
+                    {
+                        DrawTriangle({1680 , 970}, {1680 , 1030} , {1730 , 1000} , BLACK);
+                        if(CheckCollisionPointCircle(mishka, {1700 , 1000} , 60))
+                        {
+                            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                            {
+                                pause = false;
+                            }
+                        }
+                    }
+                }
+            }
             
             if (debug == true)
             {
@@ -245,6 +349,7 @@ int main(void)
             {
                 for(int i = 0; i <= max_files ; i++)
                 {
+
                     DrawText(TextFormat("Files: %i", music_files.count), 190, 250, 20, WHITE);
                     //DrawText(TextFormat("Files: %i", files_names),1400, 200, 20, RED);
                 }
@@ -256,10 +361,19 @@ int main(void)
             {
                 for(int i = 0; i < music_files.count; i++)
                 {
-                    get_sound = { 1400, 190 + (i * 20) , 600, 20 };
-                    //DrawRectangleRec(get_sound, BLANK);
-                    DrawRectangle(1400 , 200 + (i * 20), 600, 20 , BLANK);
-                    DrawText(music_files.paths[i], 1400, 200 + (i * 20), 20, WHITE);
+                    if(low_res)
+                    {
+                        get_sound = { 1000, 140 + (i * 20) , 600, 20 };
+                        DrawRectangle(1000 , 150 + (i * 20), 600, 20 , BLANK);
+                        DrawText(music_files.paths[i], 1000, 150 + (i * 20), 20, WHITE);
+                    }
+                    else
+                    {
+                        get_sound = { 1400, 190 + (i * 20) , 600, 20 };
+                        //DrawRectangleRec(get_sound, BLANK);
+                        DrawRectangle(1400 , 200 + (i * 20), 600, 20 , BLANK);
+                        DrawText(music_files.paths[i], 1400, 200 + (i * 20), 20, WHITE);
+                    }
                     
                     
                     
@@ -317,10 +431,16 @@ int main(void)
             %.2f   // 123.45
             %.3f   // 123.456
             */
-            
-            DrawText(TextFormat("Playing for: %.1f" , GetMusicTimePlayed(muzika)),190, 350, 20, WHITE );
-            DrawText(TextFormat("Full time of the music: %.1f" , GetMusicTimeLength(muzika)),190, 400, 20, WHITE );
-            
+            if(low_res)
+            {
+                DrawText(TextFormat("Playing for: %.1f" , GetMusicTimePlayed(muzika)),50, 350, 20, WHITE );
+                DrawText(TextFormat("Full time of the music: %.1f" , GetMusicTimeLength(muzika)),50, 400, 20, WHITE ); 
+            }
+            else
+            {
+                DrawText(TextFormat("Playing for: %.1f" , GetMusicTimePlayed(muzika)),190, 350, 20, WHITE );
+                DrawText(TextFormat("Full time of the music: %.1f" , GetMusicTimeLength(muzika)),190, 400, 20, WHITE );
+            }
             full_song_time = GetMusicTimeLength(muzika);
             if(GetMusicTimePlayed(muzika) >= full_song_time)
             {
